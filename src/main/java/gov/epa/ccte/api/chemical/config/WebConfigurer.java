27f1e7d453951f49205240b20764a1f975081082
Asif Rashid <rashid.asif@epa.gov>
2023-04-15 13:03:47 -0400

    fixed the cors issue with api-key

--- src/main/java/gov/epa/ccte/api/chemical/config/WebConfigurer.java
+++ src/main/java/gov/epa/ccte/api/chemical/config/WebConfigurer.java
@@ -1,16 +1,22 @@
 package gov.epa.ccte.api.chemical.config;
 
-import javax.servlet.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.DefaultCorsProcessor;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
 
 
 /**
@@ -43,14 +49,40 @@ public class WebConfigurer implements ServletContextInitializer {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = applicationProperties.getCors();
-        if (!CollectionUtils.isEmpty(config.getAllowedOrigins()) || !CollectionUtils.isEmpty(config.getAllowedOriginPatterns())) {
-            log.debug("Registering CORS filter");
-            source.registerCorsConfiguration("/api/**", config);
-            source.registerCorsConfiguration("/management/**", config);
-            source.registerCorsConfiguration("/v3/api-docs", config);
-            source.registerCorsConfiguration("/swagger-ui/**", config);
-        }
-        return new CorsFilter(source);
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setExposedHeaders(Collections.singletonList("Authorization"));
+        config.setAllowCredentials(false);
+        //config.setMaxAge(3600);
+        source.registerCorsConfiguration("/**", config);
+        CorsFilter filter = new CorsFilter(source);
+        filter.setCorsProcessor(new DefaultCorsProcessor() {
+            @Override
+            public boolean processRequest(CorsConfiguration config, HttpServletRequest request, HttpServletResponse response)
+                    throws IOException {
+                if ("OPTIONS".equals(request.getMethod()) && "/chemical/**".equals(request.getRequestURI())) {
+                    // Exclude the API endpoint from preflight checks
+                    return true;
+                }
+                return super.processRequest(config, request, response);
+            }
+        });
+        return filter;
     }
+
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = applicationProperties.getCors();
+//        if (!CollectionUtils.isEmpty(config.getAllowedOrigins()) || !CollectionUtils.isEmpty(config.getAllowedOriginPatterns())) {
+//            log.debug("Registering CORS filter");
+//            source.registerCorsConfiguration("/**", config);
+//            //source.registerCorsConfiguration("/management/**", config);
+//            source.registerCorsConfiguration("/v3/api-docs", config);
+//            source.registerCorsConfiguration("/swagger-ui/**", config);
+//        }
+//        return new CorsFilter(source);
+//    }
 }
