bdaa680e64cadf30e44ed27d7d90a2b148b26482
asif rashid <58010651+asif-rashid@users.noreply.github.com>
2024-01-17 15:34:58 -0500

    implemented endpoints for chemical search in lists #33 (#43)

---
+++ src/main/java/gov/epa/ccte/api/chemical/projection/search/DtxsidOnly.java
@@ -0,0 +1,8 @@
+package gov.epa.ccte.api.chemical.projection.search;
+
+/**
+ * Projection for {@link gov.epa.ccte.api.chemical.domain.ChemicalSearch}
+ */
+public interface DtxsidOnly {
+    String getDtxsid();
+}
--- src/main/java/gov/epa/ccte/api/chemical/repository/ChemicalListChemicalRepository.java
+++ src/main/java/gov/epa/ccte/api/chemical/repository/ChemicalListChemicalRepository.java
@@ -13,4 +13,15 @@ public interface ChemicalListChemicalRepository extends JpaRepository<ChemicalLi
 
     @Query("select distinct c.listName from ChemicalListChemical c where c.dtxsid = ?1")
     List<String> getListNames(String dtxsid);
+
+    @Query(nativeQuery = true,
+    value = " select distinct s.dtxsid from ms.chemical_search s join ch.v_chemical_list_chemicals l on l.dtxsid = s.dtxsid " +
+            " where s.modified_value like :word% and l.list_name = :list ")
+    List<String> startWith(String word, String list);
+
+    @Query(nativeQuery = true,
+            value = " select distinct s.dtxsid from ms.chemical_search s join ch.v_chemical_list_chemicals l on l.dtxsid = s.dtxsid " +
+                    " where s.modified_value like %:word% and l.list_name = :list ")
+    List<String> contain(String word, String list);
+
 }
