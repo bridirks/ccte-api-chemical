34a5f5fbf3aba33b1d1561d6feb906d59732e6a4
asif rashid <rashid.asif@epa.gov>
2024-02-28 18:11:35 -0500

    fixed CASRN dashes issue and bug related to inchikey and start-with #55

--- src/main/java/gov/epa/ccte/api/chemical/security/ApiKeyRequestFilter.java
+++ src/main/java/gov/epa/ccte/api/chemical/security/ApiKeyRequestFilter.java
@@ -73,7 +73,7 @@ public class ApiKeyRequestFilter extends GenericFilterBean {
 
         // dump the request headers
         log.info("*** request headers ***");
-        Enumeration<String> headerNames = ((HttpServletRequest) servletRequest).getHeaderNames();
+//        Enumeration<String> headerNames = ((HttpServletRequest) servletRequest).getHeaderNames();
 
 //        if (headerNames != null) {
 //            while (headerNames.hasMoreElements()) {
@@ -160,7 +160,7 @@ public class ApiKeyRequestFilter extends GenericFilterBean {
     }
 
 
-    private String getApiKeyFromQueryParam(String query) throws UnsupportedEncodingException {
+    private String getApiKeyFromQueryParam(String query){
 
         // an example -  format=svg&x-api-key=f1d96bdd-223a-434e-b1c0-af373a59a19e
 
