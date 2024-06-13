02264aebb1cc016a361d9292c30c39e45d6ee69b
asif rashid <rashid.asif@epa.gov>
2024-06-13 11:49:12 -0400

    Added batch endpoint for safety data #86

--- src/main/java/gov/epa/ccte/api/chemical/web/rest/GhsLinkResource.java
+++ src/main/java/gov/epa/ccte/api/chemical/web/rest/GhsLinkResource.java
@@ -4,11 +4,17 @@ import gov.epa.ccte.api.chemical.repository.ChemicalDetailRepository;
 import gov.epa.ccte.api.chemical.repository.ChemicalListChemicalRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * REST controller for checking availability of data in Pubchem for GHS Safety data
@@ -34,17 +40,26 @@ public class GhsLinkResource {
     @RequestMapping(value = "chemical/ghslink/to-dtxsid/{dtxsid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     GhsLinkResponse byDtxsid(@Parameter(required = true, description = "DSSTox Substance Identifier", example = "DTXSID7020182") @PathVariable("dtxsid") String dtxsid) {
-        if (chemicalListChemicalRepository.isGhsLinkExists(dtxsid))
-            return new GhsLinkResponse(dtxsid,true, getSafetyUrl(dtxsid));
-        else
+
+        log.debug("Received request to check existence by dtxsid: {} ", dtxsid);
+
+        if(chemicalListChemicalRepository.isGhsLinkExists(new String[]{dtxsid}).isEmpty())
             return new GhsLinkResponse(dtxsid, false, null);
-
-        //return chemicalListChemicalRepository.isGhsLinkExists(dtxsid);
+        else
+            return chemicalListChemicalRepository.isGhsLinkExists(new String[]{dtxsid}).get(0);
     }
 
-    private String getSafetyUrl(String dtxsid) {
-        String inchikey = chemicalDetailRepository.getInchikeyForDtxsid(dtxsid).get();
+    @Operation(summary = "Check existence for batch of dtxsid", description = "This endpoint will return Y if Pubchem has GHS Safety data otherwise it will return N.")
+    @RequestMapping(value = "chemical/ghslink/to-dtxsid/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    List<GhsLinkResponse> byBatchDtxsid(@io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, description = "JSON array of DSSTox Substance Identifier",
+            content = {@Content(array = @ArraySchema(schema = @Schema(implementation = String.class)),
+                    examples = {@ExampleObject("\"[\\\"DTXSID7020182\\\",\\\"DTXSID9020112\\\"]\"")})})
+                                  @RequestBody String[] dtxsids) {
 
-        return "https://pubchem.ncbi.nlm.nih.gov/" + inchikey + "#section=GHS-Classification";
+        log.debug("Received request to check existence by {} dtxsid: ", dtxsids.length);
+
+        return chemicalListChemicalRepository.isGhsLinkExists(dtxsids);
     }
+
 }
