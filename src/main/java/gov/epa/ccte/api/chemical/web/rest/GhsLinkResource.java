d2b6d3afc5034586179c70fbff6c3794a58949b7
bedirks <Dirks.Brianna@epa.gov>
2024-10-10 14:05:01 -0400

    implement both GET and POST methods for new Wikipedia search endpoint #109

--- src/main/java/gov/epa/ccte/api/chemical/web/rest/GhsLinkResource.java
+++ src/main/java/gov/epa/ccte/api/chemical/web/rest/GhsLinkResource.java
@@ -1,6 +1,5 @@
 package gov.epa.ccte.api.chemical.web.rest;
 
-import gov.epa.ccte.api.chemical.repository.ChemicalDetailRepository;
 import gov.epa.ccte.api.chemical.repository.ChemicalListChemicalRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
