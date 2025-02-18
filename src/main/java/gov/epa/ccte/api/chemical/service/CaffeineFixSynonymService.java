068225e542c4783a2bb73d421cc7dbab2ba8a83d
okemghou <kemghou.odile@epa.com>
2025-02-18 10:42:09 -0500

    API deployment

--- src/main/java/gov/epa/ccte/api/chemical/service/CaffeineFixSynonymService.java
+++ src/main/java/gov/epa/ccte/api/chemical/service/CaffeineFixSynonymService.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Slf4j
@@ -21,7 +24,22 @@ public class CaffeineFixSynonymService {
 
     @PostConstruct
     private void init() throws IOException {
-        caffeineFix = new CaffeineFix(dictionaryResource.getFile().getPath());
+        // Create a temporary file
+        File tempFile = File.createTempFile("synonym", ".cfx");
+        tempFile.deleteOnExit(); // Ensure it gets deleted on exit
+
+        // Write the contents of the resource to the temporary file
+        try (InputStream inputStream = dictionaryResource.getInputStream();
+             FileOutputStream outputStream = new FileOutputStream(tempFile)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        }
+
+        // Initialize CaffeineFix with the path of the temporary file
+        caffeineFix = new CaffeineFix(tempFile.getAbsolutePath());
     }
 
     public List<String> caffeineFix(String nameString){
