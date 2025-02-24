55108656442911728060e398069596d0a9cd512f
SrikanthDeevi <62434984+SrikanthDeevi@users.noreply.github.com>
2025-02-24 09:01:16 -0500

    CE-4501- Moved GhslinkAPI  and fate Open api tags to interface (#167)
    * CE-4501- Moved GhslinkAPI Open api tags to interface
    
    * CE-4502-Moved GhslinkAPI Open api tags to interface

--- src/main/java/gov/epa/ccte/api/chemical/web/rest/GhsLinkResource.java
+++ src/main/java/gov/epa/ccte/api/chemical/web/rest/GhsLinkResource.java
@@ -1,61 +1,37 @@
 package gov.epa.ccte.api.chemical.web.rest;
 
 import gov.epa.ccte.api.chemical.repository.ChemicalListChemicalRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * REST controller for checking availability of data in Pubchem for GHS Safety data
+ * REST controller for checking availability of GHS Safety data in PubChem.
  */
-@Tag(name = "Pubchem link to GHS Classification ",
-        description = "API endpoints for checking if chemical has GHS classification safety data in Pubchem. ")
-@SecurityRequirement(name = "api_key") // no need for api_key for this endpoint
 @Slf4j
 @RestController
-public class GhsLinkResource {
+public class GhsLinkResource implements GhsLinkApi {
 
-    final private ChemicalListChemicalRepository chemicalListChemicalRepository;
+    private final ChemicalListChemicalRepository chemicalListChemicalRepository;
 
     public GhsLinkResource(ChemicalListChemicalRepository chemicalListChemicalRepository) {
-
         this.chemicalListChemicalRepository = chemicalListChemicalRepository;
     }
 
-    @Operation(summary = "Check existence by dtxsid", description = "This endpoint will return Y if Pubchem has GHS Safety data otherwise it will return N.")
-    @RequestMapping(value = "chemical/ghslink/to-dtxsid/{dtxsid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    GhsLinkResponse byDtxsid(@Parameter(required = true, description = "DSSTox Substance Identifier", example = "DTXSID7020182") @PathVariable("dtxsid") String dtxsid) {
-
+    @Override
+    public GhsLinkResponse byDtxsid(String dtxsid) {
         log.debug("Received request to check existence by dtxsid: {} ", dtxsid);
 
-        if(chemicalListChemicalRepository.isGhsLinkExists(new String[]{dtxsid}).isEmpty())
+        if (chemicalListChemicalRepository.isGhsLinkExists(new String[]{dtxsid}).isEmpty()) {
             return new GhsLinkResponse(dtxsid, false, null);
-        else
+        } else {
             return chemicalListChemicalRepository.isGhsLinkExists(new String[]{dtxsid}).get(0);
+        }
     }
 
-    @Operation(summary = "Check existence for batch of dtxsid", description = "This endpoint will return Y if Pubchem has GHS Safety data otherwise it will return N.")
-    @RequestMapping(value = "chemical/ghslink/to-dtxsid/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    List<GhsLinkResponse> byBatchDtxsid(@io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, description = "JSON array of DSSTox Substance Identifier",
-            content = {@Content(array = @ArraySchema(schema = @Schema(implementation = String.class)),
-                    examples = {@ExampleObject("\"[\\\"DTXSID7020182\\\",\\\"DTXSID9020112\\\"]\"")})})
-                                  @RequestBody String[] dtxsids) {
-
+    @Override
+    public List<GhsLinkResponse> byBatchDtxsid(String[] dtxsids) {
         log.debug("Received request to check existence by {} dtxsid: ", dtxsids.length);
-
         return chemicalListChemicalRepository.isGhsLinkExists(dtxsids);
     }
-
 }
