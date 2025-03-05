911d6bef001cec6c7d4ce168ea5e11219dd6981b
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-03-05 14:27:38 -0500

    Create chemical property model endpoints #150 (#173)

---
+++ src/main/java/gov/epa/ccte/api/chemical/repository/ModelFilesRepository.java
@@ -0,0 +1,15 @@
+package gov.epa.ccte.api.chemical.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import gov.epa.ccte.api.chemical.domain.ModelFiles;
+
+public interface ModelFilesRepository extends JpaRepository<ModelFiles, Long>{
+    
+    @Transactional(readOnly = true)
+    @Query(nativeQuery = true,value = "select c.file_bytes from chemprop.mv_model_files c where c.model_id = :modelId and c.file_type_id = :typeId")
+    byte[] getFileImageForModelIdAndTTypeId(@Param("modelId") Integer modelId, @Param("typeId") Integer typeId);
+}
