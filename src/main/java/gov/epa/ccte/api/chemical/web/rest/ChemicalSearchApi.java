2df6c798cf98411c9067494935dbe272fe8c15dc
SrikanthDeevi <deevi.srikanth@epa.gov>
2025-03-09 22:59:58 -0400

    CE-4526 Added interface for chemical search resource

--- src/main/java/gov/epa/ccte/api/chemical/web/rest/ChemicalSearchApi.java
+++ src/main/java/gov/epa/ccte/api/chemical/web/rest/ChemicalSearchApi.java
@@ -1,6 +1,14 @@
 package gov.epa.ccte.api.chemical.web.rest;
 
-import gov.epa.ccte.api.chemical.projection.search.*;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import gov.epa.ccte.api.chemical.projection.search.ChemicalSearchAll;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,71 +18,72 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
-/**
- * API interface for searching chemicals.
- */
-@Tag(name = "Chemical Search Resource",
-        description = "API endpoints for searching chemicals using different identifiers or characteristics.")
+@Tag(name = "Chemical Search Resource", description = "API endpoints for searching chemicals using different identifiers or characteristics.")
 @SecurityRequirement(name = "api_key")
-@RequestMapping(value = "chemical/search", produces = MediaType.APPLICATION_JSON_VALUE)
-public interface ChemicalSearchApi {
+public interface ChemicalSearchApi{
 
-    @Operation(summary = "Search by starting value", description = "NOTE: Search value needs to be URL encoded for synonyms.")
-    @GetMapping(value = "/start-with/{word}")
-    List<ChemicalSearchAll> chemicalStartWith(@Parameter(required = true, description = "Starting characters for search word")
-                                             @PathVariable("word") String word,
-                                             @RequestParam(value = "top", required = false, defaultValue = "500") Integer top);
-
-    @Operation(summary = "Search by exact value", description = "NOTE: Search value needs to be URL encoded for synonyms.")
-    @GetMapping(value = "/equal/{word}")
-    List chemicalEqual(@Parameter(required = true, description = "Exact match of search word")
-                        @PathVariable("word") String word,
-                        @RequestParam(value = "projection", required = false, defaultValue = "chemicalsearchall") String projection);
-
-    @Operation(summary = "Search by exact batch of values", description = "NOTE: Search batch of values (values are separated by EOL character and maximum 200 values are allowed).")
-    @ApiResponses(value= {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json",
-                    schema=@Schema(oneOf = {ChemicalSearchAll.class})))
+    @Operation(summary = "Search by starting value", description = "NOTE: Search value needs to be URL encoded for synonyms")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(oneOf = {ChemicalSearchAll.class}))),
+            @ApiResponse(responseCode = "400", description = "Data not found.",
+                    content = @Content(mediaType = "application/problem+json", schema = @Schema(oneOf = {ProblemDetail.class})))
     })
-    @PostMapping(value = "/equal/")
-    List<ChemicalBatchSearchResult> chemicalBatchEqual(@Parameter(required = true, description = "Exact match of batch of search words (separated by end of line character)",
-            examples = {@ExampleObject(name="DSSTox Substance Identifier", value = "DTXSID7020182"),
-                    @ExampleObject(name="DSSTox Compound Identifier", value = "DTXCID505"),
-                    @ExampleObject(name="Synonym", value = "atrazine"),
-                    @ExampleObject(name="CASRN", value = "1912-24-9"),
-                    @ExampleObject(name="InChIKey", value = "MXWJVTOOROXGIU-UHFFFAOYSA-N")})
-            @RequestBody String words);
+    @GetMapping(value = "chemical/search/start-with/{word}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<ChemicalSearchAll> chemicalStartWith(
+            @Parameter(required = true, description = "Starting characters for search word",
+                    examples = {@ExampleObject(name="DSSTox Substance Identifier", value = "DTXSID7020182"),
+                            @ExampleObject(name="CASRN", value = "1912-24")})
+            @PathVariable("word") String word,
+            @RequestParam(value = "top", required = false, defaultValue = "500") Integer top);
 
-    @Operation(summary = "Search by substring value", description = "NOTE: Search value needs to be URL encoded for synonyms.")
-    @GetMapping(value = "/contain/{word}")
-    List chemicalContain(@Parameter(required = true, description = "Substring of search word")
-                          @PathVariable("word") String word,
-                          @RequestParam(value = "top", required = false, defaultValue = "0") Integer top,
-                          @RequestParam(value = "projection", required = false, defaultValue = "chemicalsearchall") String projection);
+    @Operation(summary = "Search by exact value")
+    @GetMapping(value = "chemical/search/equal/{word}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List chemicalEqual(
+            @Parameter(required = true, description = "Exact match of search word",
+                    examples = {@ExampleObject(name="DSSTox Substance Identifier", value = "DTXSID7020182"),
+                            @ExampleObject(name="CASRN", value = "1912-24-9")})
+            @PathVariable("word") String word,
+            @RequestParam(value = "projection", required = false, defaultValue = "chemicalsearchall") String projection);
+
+    @Operation(summary = "Search by substring value")
+    @GetMapping(value = "chemical/search/contain/{word}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List chemicalContain(
+            @Parameter(required = true, description = "Substring of search word",
+                    examples = {@ExampleObject(name="DSSTox Compound Identifier", value = "DTXCID505"),
+                            @ExampleObject(name="Synonym", value = "razine")})
+            @PathVariable("word") String word,
+            @RequestParam(value = "top", required = false, defaultValue = "0") Integer top,
+            @RequestParam(value = "projection", required = false, defaultValue = "chemicalsearchall") String projection);
 
     @Operation(summary = "Search ms ready chemicals by formula")
-    @GetMapping(value = "/msready/search/by-formula/{formula}")
-    List<String> msReadyByFormula(@Parameter(required = true, description = "Chemical formula") @PathVariable("formula") String formula);
+    @GetMapping(value = "chemical/msready/search/by-formula/{formula}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<String> msReadyByFormula(@Parameter(required = true, description = "Chemical formula", example = "C16H24N2O5S") @PathVariable("formula") String formula);
 
     @Operation(summary = "Search ms ready chemicals by DTXCID")
-    @GetMapping(value = "/msready/search/by-dtxcid/{dtxcid}")
-    List<String> msReadyByDtxcid(@Parameter(required = true, description = "DSSTox Compound Identifier") @PathVariable("dtxcid") String dtxcid);
+    @GetMapping(value = "chemical/msready/search/by-dtxcid/{dtxcid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<String> msReadyByDtxcid(@Parameter(required = true, description = "DSSTox Compound Identifier", example = "DTXCID30182") @PathVariable("dtxcid") String dtxcid);
 
     @Operation(summary = "Search ms ready chemical using mass range")
-    @GetMapping(value = "/msready/search/by-mass/{start}/{end}")
-    List<String> msReadyByMass(@Parameter(required = true, description = "Starting mass value") @PathVariable("start") Double start,
-                                @Parameter(required = true, description = "Ending mass value") @PathVariable("end") Double end);
+    @GetMapping(value = "chemical/msready/search/by-mass/{start}/{end}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<String> msReadyByMass(@Parameter(required = true, description = "Starting mass value", example = "200.90") @PathVariable("start") Double start,
+                               @Parameter(required = true, description = "Ending mass value", example = "200.95") @PathVariable("end") Double end);
 
     @Operation(summary = "Search chemicals by exact formula")
-    @GetMapping(value = "/search/by-exact-formula/{formula}")
-    List<String> getChemicalsForExactFormula(@Parameter(required = true, description = "Chemical Formula") @PathVariable("formula") String formula);
+    @GetMapping(value = "chemical/search/by-exact-formula/{formula}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<String> getChemicalsForExactFormula(@Parameter(required = true, description = "Chemical formula", example = "C15H16O2") @PathVariable("formula") String formula);
 
     @Operation(summary = "Search chemicals Count by exact formula")
-    @GetMapping(value = "/count/by-exact-formula/{formula}")
-    Long getChemicalsCountForExactFormula(@Parameter(required = true, description = "Chemical Formula") @PathVariable("formula") String formula,
-                                           @RequestParam(value = "projection", required = false, defaultValue = "count") String projection);
+    @GetMapping(value = "chemical/count/by-exact-formula/{formula}")
+    Long getChemicalsCountForExactFormula(@Parameter(required = true, description = "Chemical formula", example = "C15H16O2") @PathVariable("formula") String formula,
+                                          @RequestParam(value = "projection", required = false, defaultValue = "count") String projection);
+
+    @Operation(summary = "Search chemicals by ms ready formula")
+    @GetMapping(value = "chemical/search/by-msready-formula/{formula}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<String> getChemicalsForMsreadyFormula(@Parameter(required = true, description = "Chemical formula", example = "C15H16O2") @PathVariable("formula") String formula);
+
+    @Operation(summary = "Search chemicals Count by ms ready formula")
+    @GetMapping(value = "chemical/count/by-msready-formula/{formula}")
+    Long getChemicalsCountForMsreadyFormula(@Parameter(required = true, description = "Chemical formula", example = "C15H16O2") @PathVariable("formula") String formula,
+                                            @RequestParam(value = "projection", required = false, defaultValue = "count") String projection);
 }
--- src/main/java/gov/epa/ccte/api/chemical/web/rest/ChemicalSearchResource.java
+++ src/main/java/gov/epa/ccte/api/chemical/web/rest/ChemicalSearchResource.java
@@ -1,24 +1,20 @@
 package gov.epa.ccte.api.chemical.web.rest;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.epa.ccte.api.chemical.projection.search.ChemicalBatchSearchResult;
 import gov.epa.ccte.api.chemical.projection.search.ChemicalSearchAll;
-import gov.epa.ccte.api.chemical.projection.search.ChemicalSearchInternal;
+import gov.epa.ccte.api.chemical.projection.search.DtxsidOnly;
 import gov.epa.ccte.api.chemical.repository.ChemicalSearchRepository;
 import gov.epa.ccte.api.chemical.service.SearchChemicalService;
 import gov.epa.ccte.api.chemical.web.rest.errors.ChemicalSearchNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * REST controller for chemical search operations.
- */
 @Slf4j
 @RestController
 public class ChemicalSearchResource implements ChemicalSearchApi {
-
     private final ChemicalSearchRepository searchRepository;
     private final SearchChemicalService chemicalService;
 
@@ -31,7 +27,12 @@ public class ChemicalSearchResource implements ChemicalSearchApi {
     public List<ChemicalSearchAll> chemicalStartWith(String word, Integer top) {
         List<ChemicalSearchAll> searchResult = chemicalService.getStartWith(word, top);
         if (!searchResult.isEmpty()) {
-            return searchResult.subList(0, Math.min(searchResult.size(), top));
+            if (top != null && top > 0) {
+                log.debug("picking up top {} records", top);
+                return searchResult.stream().limit(top).collect(Collectors.toList());
+            } else {
+                return searchResult;
+            }
         } else {
             throw new ChemicalSearchNotFoundException(chemicalService.getErrorMsgs(word), chemicalService.getSuggestions(word));
         }
@@ -40,53 +41,79 @@ public class ChemicalSearchResource implements ChemicalSearchApi {
     @Override
     public List chemicalEqual(String word, String projection) {
         String searchWord = chemicalService.preprocessingSearchWord(word);
-        List searchResult = searchRepository.findByModifiedValueOrderByRankAsc(searchWord, ChemicalSearchAll.class);
-        if (searchResult.isEmpty()) {
-            throw new ChemicalSearchNotFoundException(chemicalService.getErrorMsgs(word), chemicalService.getSuggestions(word));
+        log.debug("input search word = {} and process search word = {}. ", word, searchWord);
+        List searchResult = null;
+        switch (projection) {
+            case "chemicalsearchall":
+                searchResult = searchRepository.findByModifiedValueOrderByRankAsc(searchWord, ChemicalSearchAll.class);
+                searchResult = chemicalService.removeDuplicates(searchResult);
+                break;
+            case "dtxsidonly":
+                searchResult = searchRepository.findByModifiedValueOrderByRankAsc(searchWord, DtxsidOnly.class);
+                searchResult = chemicalService.removeDuplicates(searchResult);
+                break;
+            case "ccdsearchresult":
+                searchResult = searchRepository.equalCcd(searchWord);
+                searchResult = chemicalService.removeDuplicates(searchResult);
+                break;
         }
-        return searchResult;
-    }
-
-    @Override
-    public List<ChemicalBatchSearchResult> chemicalBatchEqual(String words) {
-        String[] searchWords = chemicalService.preprocessingSearchWord(words.split("\n"));
-        log.debug("input search words = {} and process search word count = {}. ", words, searchWords.length);
-        List<ChemicalSearchInternal> searchResult = searchRepository.findByModifiedValueInOrderByRankAsc(List.of(searchWords), ChemicalSearchInternal.class);
-        return chemicalService.processBatchResult(searchResult, searchWords);
+        if (searchResult == null || searchResult.isEmpty())
+            throw new ChemicalSearchNotFoundException(chemicalService.getErrorMsgs(word), chemicalService.getSuggestions(word));
+        else
+            return searchResult;
     }
 
     @Override
     public List chemicalContain(String word, Integer top, String projection) {
         String searchWord = chemicalService.preprocessingSearchWord(word);
+        log.debug("input search word = {} and process search word = {}. projection = {}", word, searchWord, projection);
         List searchResult = chemicalService.getContain(projection, searchWord, top);
-        if (searchResult.isEmpty()) {
+        searchResult = chemicalService.removeDuplicates(searchResult);
+        if (searchResult == null || searchResult.isEmpty())
             throw new ChemicalSearchNotFoundException(chemicalService.getErrorMsgs(word), chemicalService.getSuggestions(word));
-        }
-        return searchResult;
+        else
+            return searchResult;
     }
 
     @Override
     public List<String> msReadyByFormula(String formula) {
+        log.debug("input formula = {} ", formula);
         return searchRepository.searchMsReadyFormula(formula);
     }
 
     @Override
     public List<String> msReadyByDtxcid(String dtxcid) {
+        log.debug("input dtxcid = {} ", dtxcid);
         return searchRepository.searchMsReadyDtxcid(dtxcid);
     }
 
     @Override
     public List<String> msReadyByMass(Double start, Double end) {
+        log.debug("input mass = {} - {} ", start, end);
         return searchRepository.searchMsReadyMass(start, end);
     }
 
     @Override
     public List<String> getChemicalsForExactFormula(String formula) {
+        log.debug("exact formula search for {} ", formula);
         return searchRepository.getExactFormula(formula);
     }
 
     @Override
     public Long getChemicalsCountForExactFormula(String formula, String projection) {
+        log.debug("exact formula count for {} ", formula);
         return searchRepository.getExactFormulaCount(formula);
+    }
+
+    @Override
+    public List<String> getChemicalsForMsreadyFormula(String formula) {
+        log.debug("input formula {} ", formula);
+        return searchRepository.searchAllMsReadyFormula(formula);
+    }
+
+    @Override
+    public Long getChemicalsCountForMsreadyFormula(String formula, String projection) {
+        log.debug("formula count for {} ", formula);
+        return searchRepository.getMsReadyFormulaCount(formula);
     }
 }
