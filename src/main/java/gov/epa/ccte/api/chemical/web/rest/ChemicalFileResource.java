ee4437bc91ef32767abc61b195c9048f6a0b5bef
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-03-18 14:00:45 -0400

    Fix ChemicalFilesResource and ChemicalFilesAPi #187 (#188)

--- src/main/java/gov/epa/ccte/api/chemical/web/rest/ChemicalFileResource.java
+++ src/main/java/gov/epa/ccte/api/chemical/web/rest/ChemicalFileResource.java
@@ -1,20 +1,19 @@
 package gov.epa.ccte.api.chemical.web.rest;
 
+import gov.epa.ccte.api.chemical.domain.ImageFormat;
 import gov.epa.ccte.api.chemical.repository.ChemicalDetailRepository;
 import gov.epa.ccte.api.chemical.service.ChemicalImageUtils;
-import gov.epa.ccte.api.chemical.domain.ImageFormat;
 import gov.epa.ccte.api.chemical.web.rest.errors.IdentifierNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
 
 import static org.springframework.http.MediaType.IMAGE_PNG;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 
 /**
- * REST controller for retrieving chemical structure data.
+ * REST controller for retrieving chemical structure data and for getting the {@link gov.epa.ccte.api.chemical.domain.ChemicalDetail}s.
  */
 @Slf4j
 @RestController
@@ -77,6 +76,65 @@ public class ChemicalFileResource implements ChemicalFileApi {
             return ResponseEntity.ok().contentType(MediaType.valueOf("image/svg+xml")).body(image);
         } else {
             return null;
+        }
+    }
+    
+    @Override
+    public ResponseEntity<String> molByDtxsid( String dtxsid){
+        log.debug("dtxsid = {}", dtxsid);
+
+        String mol = detailRepository.getMolFileForDtxsid(dtxsid)
+                .orElseThrow(()->new IdentifierNotFoundException("DTXSID", dtxsid));
+
+        return ResponseEntity.ok().contentType(TEXT_PLAIN).body(mol);
+    }
+    
+    @Override
+    public ResponseEntity<String> molByDtxcid( String dtxcid){
+        log.debug("dtxsid = {}", dtxcid);
+
+        String mol = detailRepository.getMolFileForDtxcid(dtxcid)
+                .orElseThrow(()->new IdentifierNotFoundException("DTXCID", dtxcid));
+
+        return ResponseEntity.ok().contentType(TEXT_PLAIN).body(mol);
+    }
+    
+    @Override
+    public ResponseEntity<String> mrvByDtxsid( String dtxsid){
+        log.debug("dtxsid = {}", dtxsid);
+
+        String mol = detailRepository.getMrvFileForDtxsid(dtxsid)
+                .orElseThrow(()->new IdentifierNotFoundException("DTXSID", dtxsid));
+
+        return ResponseEntity.ok().contentType(TEXT_PLAIN).body(mol);
+    }
+    
+    @Override
+    public ResponseEntity<String> mrvByDtxcid( String dtxcid){
+        log.debug("dtxsid = {}", dtxcid);
+
+        String mol = detailRepository.getMrvFileForDtxcid(dtxcid)
+                .orElseThrow(()->new IdentifierNotFoundException("DTXCID", dtxcid));
+
+        return ResponseEntity.ok().contentType(TEXT_PLAIN).body(mol);
+    }
+    
+    @Override
+    public ResponseEntity<byte[]> generateImageBySmiles( String smiles, ImageFormat format){
+        log.debug("smiles = {}, image type={}", smiles, format);
+
+        byte[] image = ChemicalImageUtils.smileToImage(smiles, format);
+
+        switch (format){
+            case PNG -> {
+                return ResponseEntity.ok().contentType(IMAGE_PNG).body(image);
+            }
+            case SVG -> {
+                return ResponseEntity.ok().contentType(MediaType.valueOf("image/svg+xml")).body(image);
+            }
+            default -> {
+                return null;
+            }
         }
     }
 }
