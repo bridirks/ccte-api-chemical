4dc9e6ac14564d416174ff4d5c96286cff5edcda
Asif Rashid <rashid.asif@epa.gov>
2025-03-25 08:39:18 -0400

    Disable REST export for ModelFiles and ModelReports repositories

--- src/main/java/gov/epa/ccte/api/chemical/repository/ModelFilesRepository.java
+++ src/main/java/gov/epa/ccte/api/chemical/repository/ModelFilesRepository.java
@@ -3,10 +3,12 @@ package gov.epa.ccte.api.chemical.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import gov.epa.ccte.api.chemical.domain.ModelFiles;
 
+@RepositoryRestResource(exported = false)
 public interface ModelFilesRepository extends JpaRepository<ModelFiles, Long>{
     
     @Transactional(readOnly = true)
--- src/main/java/gov/epa/ccte/api/chemical/repository/ModelReportsRepository.java
+++ src/main/java/gov/epa/ccte/api/chemical/repository/ModelReportsRepository.java
@@ -3,10 +3,12 @@ package gov.epa.ccte.api.chemical.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import gov.epa.ccte.api.chemical.domain.ModelReports;
 
+@RepositoryRestResource(exported = false)
 public interface ModelReportsRepository extends JpaRepository<ModelReports, Long>{
 
     @Transactional(readOnly = true)
